a8610a24061aafd40007faa5613c39d03bd8417e
Francisco Sabino <fdasabino@gmail.com>
2022-12-09 10:35:41 +0100

    added more lessons

--- JS-103 - bank_website/script.js
+++ JS-103 - bank_website/script.js
@@ -1,19 +1,20 @@
 "use strict";
 
-// Modal window
-
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+const navLinksCont = document.querySelector(".nav__links");
 
-const openModal = function (event) {
+const openModal = (event) => {
   event.preventDefault();
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
 };
 
-const closeModal = function () {
+const closeModal = () => {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
 };
@@ -25,8 +26,30 @@ btnsOpenModal.forEach((btn) => {
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
 
-document.addEventListener("keydown", function (e) {
+document.addEventListener("keydown", (e) => {
   if (e.key === "Escape" && !modal.classList.contains("hidden")) {
     closeModal();
+  }
+});
+
+btnScrollTo.addEventListener("click", (e) => {
+  e.preventDefault();
+  section1.scrollIntoView({
+    behavior: "smooth",
+  });
+});
+
+// Page smooth scrolling navigation with event delegation
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the event
+navLinksCont.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({
+      behavior: "smooth",
+    });
   }
 });
--- JS-104 - advanced DOM events/script.js
+++ JS-104 - advanced DOM events/script.js
@@ -10,12 +10,12 @@ console.log(document.body);
 const allSections = document.querySelectorAll(".section");
 console.log(allSections);
 
-const header = document.querySelector(".header");
-console.log(header);
-
 console.log(document.getElementById("footer"));
 console.log(document.getElementsByClassName("section"));
 console.log(document.getElementsByTagName("section"));
+
+const header = document.querySelector(".header");
+console.log(header);
 
 // Creating and inserting elements
 const messageContainer = document.createElement("div");
@@ -30,3 +30,44 @@ const buttonCookie = document.querySelector(".btn--close-cookie");
 buttonCookie.addEventListener("click", () => {
   messageContainer.remove();
 });
+
+// Old way
+// scrolling
+// window.scrollTo(
+//   section1Coords.left + window.pageXOffset,
+//   section1Coords.top + window.pageYOffset
+// );
+
+// window.scrollTo({
+//   left: section1Coords.left + window.pageXOffset,
+//   top: section1Coords.top + window.pageYOffset,
+//   behavior: "smooth",
+// });
+
+// creating a random color in rgb
+// const randomInt = (min, max) => {
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// };
+
+// const randomColor = () => {
+//   return `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(
+//     0,
+//     255
+//   )})`;
+// };
+
+// const link = document
+//   .querySelector(".nav__link")
+//   .addEventListener("click", (e) => {
+//     console.log("Link =>", e.currentTarget);
+//     stop propagation
+//     e.stopPropagation();
+//   });
+
+// document.querySelector(".nav__links").addEventListener("click", (e) => {
+//   console.log("Links Cont =>", e.currentTarget);
+// });
+
+// document.querySelector(".nav").addEventListener("click", (e) => {
+//   console.log("nav =>", e.currentTarget);
+// });
