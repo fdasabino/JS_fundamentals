86033cdefca7d227431ab247c010b90b1b207fd2
Francisco Sabino <fdasabino@gmail.com>
2022-12-09 12:43:34 +0100

    added more lessons

--- JS-103 - bank_website/script.js
+++ JS-103 - bank_website/script.js
@@ -7,7 +7,12 @@ const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 const btnScrollTo = document.querySelector(".btn--scroll-to");
 const section1 = document.querySelector("#section--1");
 const navLinksCont = document.querySelector(".nav__links");
+const h1 = document.querySelector("h1");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContent = document.querySelectorAll(".operations__content");
 
+// Functions
 const openModal = (event) => {
   event.preventDefault();
   modal.classList.remove("hidden");
@@ -19,6 +24,7 @@ const closeModal = () => {
   overlay.classList.add("hidden");
 };
 
+// Event handlers
 btnsOpenModal.forEach((btn) => {
   btn.addEventListener("click", openModal);
 });
@@ -52,4 +58,26 @@ navLinksCont.addEventListener("click", (e) => {
       behavior: "smooth",
     });
   }
+});
+
+// Tabbed component
+tabsContainer.addEventListener("click", (e) => {
+  const clicked = e.target.closest(".operations__tab");
+
+  // Guard clause
+  if (!clicked) return;
+
+  // removing active classes
+  tabs.forEach((tab) => tab.classList.remove("operations__tab--active"));
+  tabsContent.forEach((tab) =>
+    tab.classList.remove("operations__content--active")
+  );
+
+  // add active class to tab
+  clicked.classList.add("operations__tab--active");
+
+  // add active class to content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
 });
